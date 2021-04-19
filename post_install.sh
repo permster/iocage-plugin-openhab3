c30f754b1748577f6c2e200123d67306b656e24e
Travis <tjgollih@gmail.com>
2021-04-18 20:41:27 -0700

    Fix a few deployment issues.

--- post_install.sh
+++ post_install.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-dir=$(pwd)
-
 # The latest version of openHAB:
 VERSION=$(fetch -qo - https://dl.bintray.com/openhab/mvn/org/openhab/distro/openhab/maven-metadata.xml | grep "<version>" | grep -Eo '[0-9\.]+' | head -n 1)
 OPENHAB_SOFTWARE_URL="https://dl.bintray.com/openhab/mvn/org/openhab/distro/openhab/${VERSION}/openhab-${VERSION}.tar.gz"
@@ -17,14 +15,14 @@ echo " done."
 # Unpack the archive into the /usr/local/share directory:
 echo -n "Installing openHAB in /usr/local/share..."
 INSTALL_DIR="/usr/local/share/openhab3"
-mkdir -p ${INSTALL_DIR} /config/persistence/db4o /config/persistence/rrd4j /config/persistence/mapdb /config/backups /config/home /config/userdata/etc/scripts /config/userdata/tmp /config/etc/openhab3
+mkdir -p ${INSTALL_DIR} /config/persistence/db4o /config/persistence/rrd4j /config/persistence/mapdb /config/backups /config/home /config/log /config/userdata/etc/scripts /config/userdata/tmp /config/etc/openhab3
 tar -xzvf openhab-*.tar.gz -C ${INSTALL_DIR}
 echo " done."
 
 # remove unnecessary files
 rm openhab-*.tar.gz
 rm -r ${INSTALL_DIR}/runtime/bin/contrib
-rm ${INSTALL_DIR}/*.ps1 ${INSTALL_DIR}/*.bat ${INSTALL_DIR}/runtime/bin/*.ps1 ${INSTALL_DIR}/runtime/bin/*.bat \
+rm ${INSTALL_DIR}/*.bat ${INSTALL_DIR}/runtime/bin/*.ps1 ${INSTALL_DIR}/runtime/bin/*.bat \
     ${INSTALL_DIR}/runtime/bin/*.psm1
 
 # Create user
@@ -35,9 +33,7 @@ mv ${INSTALL_DIR}/conf/ /config/etc/openhab3/
 mv ${INSTALL_DIR}/userdata/ /config/userdata/
 
 # patch setenv
-# cp ${INSTALL_DIR}/runtime/bin/setenv ${INSTALL_DIR}/runtime/bin/setenv.orig
-cd ${INSTALL_DIR}/runtime/bin/
-patch < ${dir}/patch-runtime_bin_setenv
+patch < ${INSTALL_DIR}/runtime/bin/patch-runtime_bin_setenv
 
 # make "openhab" the owner of the install location
 chown -R openhab:openhab ${INSTALL_DIR} /config
