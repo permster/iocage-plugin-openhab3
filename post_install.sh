4fb27021277e643411e9d287cfe114194acd3ccc
Travis <tjgollih@gmail.com>
2021-04-18 20:27:48 -0700

    Fix for openhab setenv port patch

--- post_install.sh
+++ post_install.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+dir=$(pwd)
+
 # The latest version of openHAB:
 VERSION=$(fetch -qo - https://dl.bintray.com/openhab/mvn/org/openhab/distro/openhab/maven-metadata.xml | grep "<version>" | grep -Eo '[0-9\.]+' | head -n 1)
 OPENHAB_SOFTWARE_URL="https://dl.bintray.com/openhab/mvn/org/openhab/distro/openhab/${VERSION}/openhab-${VERSION}.tar.gz"
@@ -14,25 +16,34 @@ echo " done."
 
 # Unpack the archive into the /usr/local/share directory:
 echo -n "Installing openHAB in /usr/local/share..."
-mkdir -p /usr/local/share/openhab3 /config/persistence/db4o /config/persistence/rrd4j /config/persistence/mapdb /config/backups /config/home /config/userdata/etc/scripts /config/userdata/tmp /config/etc/openhab3
-tar -xzvf openhab-*.tar.gz -C /usr/local/share/openhab3
+INSTALL_DIR="/usr/local/share/openhab3"
+mkdir -p ${INSTALL_DIR} /config/persistence/db4o /config/persistence/rrd4j /config/persistence/mapdb /config/backups /config/home /config/userdata/etc/scripts /config/userdata/tmp /config/etc/openhab3
+tar -xzvf openhab-*.tar.gz -C ${INSTALL_DIR}
 echo " done."
 
-# remove the package as it no longer needed
+# remove unnecessary files
 rm openhab-*.tar.gz
+rm -r ${INSTALL_DIR}/runtime/bin/contrib
+rm ${INSTALL_DIR}/*.ps1 ${INSTALL_DIR}/*.bat ${INSTALL_DIR}/runtime/bin/*.ps1 ${INSTALL_DIR}/runtime/bin/*.bat \
+    ${INSTALL_DIR}/runtime/bin/*.psm1
 
 # Create user
 pw user add openhab -c openhab -u 235 -d /nonexistent -s /usr/bin/nologin
 
 # Move openhab files to config directory
-mv /usr/local/share/openhab3/conf/ /config/etc/openhab3/
-mv /usr/local/share/openhab3/userdata/ /config/userdata/
+mv ${INSTALL_DIR}/conf/ /config/etc/openhab3/
+mv ${INSTALL_DIR}/userdata/ /config/userdata/
+
+# patch setenv
+# cp ${INSTALL_DIR}/runtime/bin/setenv ${INSTALL_DIR}/runtime/bin/setenv.orig
+cd ${INSTALL_DIR}/runtime/bin/
+patch < ${dir}/patch-runtime_bin_setenv
 
 # make "openhab" the owner of the install location
-chown -R openhab:openhab /usr/local/share/openhab3 /config
+chown -R openhab:openhab ${INSTALL_DIR} /config
 
 #Set write permission to be able to write plugins update
-chmod 755 /usr/local/share/openhab3
+chmod -R 755 ${INSTALL_DIR} /config/userdata /config/etc/openhab3 /config/log
 
 # Start the services
 chmod u+x /etc/rc.d/openhab3
